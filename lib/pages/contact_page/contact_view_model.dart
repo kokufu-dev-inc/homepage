09c6d32c5e08e9a3390b9c590e6cd081372be494
justice28n <justice28n@gmail.com>
2024-04-11 22:41:49 +0900

    SlackのWEBHOOKURLを変更しました。

--- lib/pages/contact_page/contact_view_model.dart
+++ lib/pages/contact_page/contact_view_model.dart
@@ -38,7 +38,7 @@ class ContactViewModel extends StateNotifier<ContactViewModelState> {
     final content = contentController.text;
 
     final slack = SlackNotifier(
-      'https://hooks.slack.com/services/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',
+      'https://hooks.slack.com/services/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',
     );
 
     final response = await slack.send(
